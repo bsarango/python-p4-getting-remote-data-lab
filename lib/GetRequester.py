f9bc8beb8ba4ea5ecc4aaece2101908c16f6a369
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-04-15 18:21:12 -0400

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,15 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        list_of_data = []
+        #request is a list/array of JSON or objects/dictionaries
+        list_of_requested_data= json.loads(self.get_response_body())
+
+        for data in list_of_requested_data:
+            list_of_data.append(data)
+
+        return list_of_data
